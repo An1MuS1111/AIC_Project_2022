deb4b3c351757cec6ec6891f432593805f62f1d3
An1MuS1111 <khalidrafi1111@gmail.com>
2022-11-10 00:14:11 +0800

    alpha_animus_9.11.22

--- app/src/main/java/com/example/aicproject2022/MainActivity.java
+++ app/src/main/java/com/example/aicproject2022/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.TextView;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
     }
 }
