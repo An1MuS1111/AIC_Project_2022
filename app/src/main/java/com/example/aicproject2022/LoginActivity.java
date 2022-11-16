555295e9df6c8404c7d363d317225e1bea313a59
Nafis Abdullah <abdullahnafis20@gmail.com>
2022-11-16 22:22:36 +0800

    alpha_naffybhai_16.11.2022

---
+++ app/src/main/java/com/example/aicproject2022/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.aicproject2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView regSign = findViewById(R.id.regPage);
+
+        regSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/aicproject2022/RegisterActivity.java
@@ -0,0 +1,26 @@
+package com.example.aicproject2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView regSign = findViewById(R.id.loginPage);
+
+        regSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
